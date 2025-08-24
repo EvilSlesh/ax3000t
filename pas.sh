504bfd806682558f3b622ad53e0bc5b917916beb
Insᴓ. <sadra.imam@gmail.com>
2025-08-24 13:21:05 +0330

    Update pas.sh

--- pas.sh
+++ pas.sh
@@ -3,23 +3,22 @@
 
 LUCIBASE="http://127.0.0.1/cgi-bin/luci"
 USER="root"
-PASS="123456789"
+PASS="YOURPASSWORD"
 APPS="xray sing-box hysteria"
 
 # login and grab sysauth cookie
-COOKIE=$(curl -s -i -d "luci_username=$USER&luci_password=$PASS" $LUCIBASE/admin/passwall2 \
-         | grep -i "Set-Cookie" | grep -o 'sysauth=[^;]*')
+COOKIE=$(curl -s -i -d "luci_username=$USER&luci_password=$PASS" $LUCIBASE/admin/passwall2 | \
+         grep -i "Set-Cookie" | grep -o 'sysauth=[^;]*')
 
 for app in $APPS; do
     INFO=$(curl -s --cookie "$COOKIE" "$LUCIBASE/admin/services/passwall2/get_${app}_info")
-
-    URL=$(echo "$INFO" | jsonfilter -e '@.url')
-    SIZE=$(echo "$INFO" | jsonfilter -e '@.size')
+    URL=$(echo "$INFO" | grep -o '"url":"[^"]*"' | cut -d'"' -f4)
+    SIZE=$(echo "$INFO" | grep -o '"size":"[^"]*"' | cut -d'"' -f4)
 
     if [ -n "$URL" ] && [ -n "$SIZE" ]; then
         echo "Updating $app..."
         RES=$(curl -s --cookie "$COOKIE" "$LUCIBASE/admin/services/passwall2/update_${app}?url=$URL&size=$SIZE")
-        echo "$RES" | jsonfilter -e '@.msg'
+        echo "$RES" | grep -q '"success":true' && echo "$app updated." || echo "$app failed."
     else
         echo "No update for $app."
     fi
