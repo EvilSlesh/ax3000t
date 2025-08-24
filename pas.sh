d793ba259a8aacb5b1338527353ba20c39e26340
Insᴓ. <sadra.imam@gmail.com>
2025-08-24 13:13:03 +0330

    Update pas.sh

--- pas.sh
+++ pas.sh
@@ -2,16 +2,22 @@
 # Passwall2 core updater (latest OpenWrt + Passwall2 only)
 
 LUCIBASE="http://127.0.0.1/cgi-bin/luci"
+USER="root"
+PASS="123456789"
 APPS="xray sing-box hysteria"
-AUTH="root:123456789"   # replace YOURPASSWORD
+
+# login and grab sysauth cookie
+COOKIE=$(curl -s -i -d "luci_username=$USER&luci_password=$PASS" $LUCIBASE/admin/passwall2 | \
+         grep -i "Set-Cookie" | grep -o 'sysauth=[^;]*')
 
 for app in $APPS; do
-    INFO=$(curl -s -u $AUTH "$LUCIBASE/admin/services/passwall2/get_${app}_info")
+    INFO=$(curl -s --cookie "$COOKIE" "$LUCIBASE/admin/services/passwall2/get_${app}_info")
     URL=$(echo "$INFO" | grep -o '"url":"[^"]*"' | cut -d'"' -f4)
     SIZE=$(echo "$INFO" | grep -o '"size":"[^"]*"' | cut -d'"' -f4)
+
     if [ -n "$URL" ] && [ -n "$SIZE" ]; then
         echo "Updating $app..."
-        RES=$(curl -s -u $AUTH "$LUCIBASE/admin/services/passwall2/update_${app}?url=$URL&size=$SIZE")
+        RES=$(curl -s --cookie "$COOKIE" "$LUCIBASE/admin/services/passwall2/update_${app}?url=$URL&size=$SIZE")
         echo "$RES" | grep -q '"success":true' && echo "$app updated." || echo "$app failed."
     else
         echo "No update for $app."
